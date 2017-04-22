4cdb7d118c10c39ed339c9b1808e8a76f42351bf
Mikhail Chernienko <mike.chernienko@sharp-dev.net>
2017-04-22 22:07:59 +1000

    initial

---
+++ f-playground/Maths.fs
@@ -0,0 +1,18 @@
+﻿namespace Helpers
+
+open System.Linq
+
+module Maths = 
+    let add num1 num2 = 
+        num1 + num2
+
+module Bools =
+    let any (items: seq<int>) =
+        items.Any()
+
+    let isSumEven n1 n2 =
+        let sum = Maths.add n1 n2
+        sum % 2 = 0
+
+//module Tuples =
+//    let 
--- f-playground/Program.fs
+++ f-playground/Program.fs
@@ -1,7 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open System
+open Helpers.Maths
+open Helpers.Bools
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    //printfn "%A" argv
+    //0 // return an integer exit code
+    let print m = 
+        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + m)
+
+    print "Hi!"
+    print "2 + 2 is 4"
+    Console.ReadKey() |> ignore
+    add 1 -1
