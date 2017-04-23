add76455e1c2385e045318b0e6af99e03d7592a0
Mikhail Chernienko <mike.chernienko@sharp-dev.net>
2017-04-24 08:15:22 +1000

    sync

--- f-playground/Maths.fs
+++ f-playground/Maths.fs
@@ -2,17 +2,35 @@
 
 open System.Linq
 
-module Maths = 
+module Misc = 
     let add num1 num2 = 
         num1 + num2
 
-module Bools =
     let any (items: seq<int>) =
         items.Any()
 
     let isSumEven n1 n2 =
-        let sum = Maths.add n1 n2
+        let sum = add n1 n2
         sum % 2 = 0
 
-//module Tuples =
-//    let 
+    let addTen num =
+        (num+10, "The original value was " + num.ToString())
+
+    let addMem n1 n2 = 
+        let res = add n1 n2
+        (res , "Values was " + n1.ToString() + " and " + n2.ToString())
+
+    type Person ={
+        name: string
+        age: int
+    }
+
+    let isHim person name age = 
+        let comp = {name = name; age = age}
+        if person <> comp then
+            if person.name = comp.name then
+                "Hmm... Names are qeual"
+            else
+                "Nope, it's not him"
+        else
+            "It's him!"
--- f-playground/Program.fs
+++ f-playground/Program.fs
@@ -2,17 +2,32 @@
 // See the 'F# Tutorial' project for more help.
 
 open System
-open Helpers.Maths
-open Helpers.Bools
+open Helpers.Misc
 
 [<EntryPoint>]
 let main argv = 
     //printfn "%A" argv
     //0 // return an integer exit code
     let print m = 
-        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + m)
+        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + m.ToString())
 
-    print "Hi!"
-    print "2 + 2 is 4"
+    let sum, message = addMem 12 134
+    print sum
+    print message
+
+    let person1 = {
+        name = "Vasya"
+        age = 20
+    }
+
+    let person2 = {
+        name = "Zhopa"
+        age = 42
+    }
+
+    print (isHim person1 "Vasya" 20)
+    print (isHim person2 "Zhopa" 100)
+    print (isHim person2 "Adelaida" 1)
+
     Console.ReadKey() |> ignore
-    add 1 -1
+    0
